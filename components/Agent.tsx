c0c7cc757e5e675395af5cfc44bf33fcf00120df
Lesuuh <lesuuh01@gmail.com>
2025-04-18 00:15:18 +0100

    fix the styling error

--- components/Agent.tsx
+++ components/Agent.tsx
@@ -37,45 +37,47 @@ const Agent = ({ userName }: AgentProps) => {
               alt="user avatar"
               width={540}
               height={540}
-              className="rounded-full object-cover"
+              className="rounded-full object-cover size-[120px]"
             />
             <h3>{userName}</h3>
           </div>
         </div>
-        {messages.length > 0 && (
-          <div className="transcript-border">
-            <div className="transcript">
-              <p
-                key={lastMessage}
-                className={cn(
-                  "transition-opacity duration-500 opacity-0",
-                  "animate-fadeIn opacity-100"
-                )}
-              >
-                {lastMessage}
-              </p>
-            </div>
+      </div>
+
+      {messages.length > 0 && (
+        <div className="transcript-border">
+          <div className="transcript">
+            <p
+              key={lastMessage}
+              className={cn(
+                "transition-opacity duration-500 opacity-0",
+                "animate-fadeIn opacity-100"
+              )}
+            >
+              {lastMessage}
+            </p>
           </div>
-        )}
-        <div className="w-full flex justify-center">
-          {callStatus !== "ACTIVE" ? (
-            <button className="relative btn-call">
-              <span
-                className={cn(
-                  "absolute animate-ping rounded-full opacity-75",
-                  callStatus !== "CONNECTING" && "hidden"
-                )}
-              />
-              <span>
-                {callStatus === "INACTIVE" || callStatus === "FINISHED"
-                  ? "Call"
-                  : "..."}
-              </span>
-            </button>
-          ) : (
-            <button className="btn-disconnect">End</button>
-          )}
         </div>
+      )}
+
+      <div className="w-full flex justify-center">
+        {callStatus !== "ACTIVE" ? (
+          <button className="relative btn-call">
+            <span
+              className={cn(
+                "absolute animate-ping rounded-full opacity-75",
+                callStatus !== "CONNECTING" && "hidden"
+              )}
+            />
+            <span>
+              {callStatus === "INACTIVE" || callStatus === "FINISHED"
+                ? "Call"
+                : "..."}
+            </span>
+          </button>
+        ) : (
+          <button className="btn-disconnect">End</button>
+        )}
       </div>
     </>
   );
